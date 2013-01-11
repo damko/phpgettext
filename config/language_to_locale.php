a638a30bb4bf156c0bd863f248ba997cad3aa40d
Damiano Venturin <dam@venturin.net>
2013-01-11 22:25:32 +0100

    added language_to_locale.php

---
+++ config/language_to_locale.php
@@ -0,0 +1,9 @@
+<?php
+//matches a language to a locale
+$config['locale_lcid'] = array(
+	'english' => 'en_US',
+	'british' => 'en_UK',
+	'french' => 'fr_FR',
+	'italian' => 'it_IT',
+	'russian' => 'ru_RU'		
+);
--- helpers/phpgettext_helper.php
+++ helpers/phpgettext_helper.php
@@ -1,7 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Stores phpgettext variables in session
+ * Stores phpgettext variables into CI session.
+ * This is called by MY_Controller so that gettext support is available for any controller
  * 
  * @access		public
  * @param		none. It uses GET
@@ -43,30 +44,26 @@ function setupPhpGettext() {
 
 	//check if the user clicked on one of the language tabs
 	$language = $CI->input->get('language');
+	
 	//stores the language value in session or retrieves from session (if set)
 	if($language) {
 		if(in_array($language, $supported_locales)) {
 			$locale = $language;
 	
 			$CI->session->set_userdata('locale',$language);
-			//$test = $CI->session->userdata('locale');
+
+		} else {
+			if(!$locale = language_to_locale($language)){
+				$locale = $CI->config->item('gettextDefaultLocale'); 
+			}
+			$CI->session->set_userdata('locale',$locale);
 		}
 	} else {
 		//check if the user (who is logged in) has a preferred language
 		$preferred_language = $CI->session->userdata('preferred_language');
 		if($preferred_language) {
-			switch ($preferred_language) {
-				case 'english':
-					$locale = 'en_US';
-				break;
-
-				case 'italian' || 'italiano':
-					$locale = 'it_IT';
-				break;
-									
-				default:
-					$locale = 'en_US';
-				break;
+			if(!$locale = language_to_locale($preferred_language)){
+				$locale = $CI->config->item('gettextDefaultLocale');				
 			}
 		} else {
 			$language_session = $CI->session->userdata('locale');
@@ -76,7 +73,7 @@ function setupPhpGettext() {
 		}
 		
 		//just in case
-		if(empty($locale)) $locale = 'en_US';
+		if(empty($locale) || !$locale) $locale = 'en_US';
 	}
 
 	//if no language is saved in session loads the default set in the config file
@@ -84,6 +81,7 @@ function setupPhpGettext() {
 		$locale = $CI->config->item('gettextDefaultLocale'); //(isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
 	}
 
+	
 	$gettext_settings = array(
 			'locale' => $locale,
 			'encoding' => $encoding,
@@ -99,18 +97,47 @@ function setupPhpGettext() {
 }
 
 /**
+ * Reads the config file language_to_locale and return the LCID value of the specified language
+ * Ex. $language == 'italian' => returns 'it_IT' 
+ * 
+ * @access		public
+ * @param		string $language
+ * @return		string LCID or false
+ * 
+ * @author 		Damiano Venturin
+ * @license	GPL
+ * @since		Dec 8, 2012
+ */
+function language_to_locale($language){
+		
+	if(!is_string($language) || empty($language)) return false;
+	
+	$CI =& get_instance();
+	
+	$config = $CI->load->config('language_to_locale',true,true);
+			
+	if(!is_array($config)) {
+		log_message('debug','The config file language_to_locale is missing or broken.');
+		return false;
+	}
+
+	$locale_lcid = $config['locale_lcid'];
+	
+	if(in_array($language, array_keys($locale_lcid))){
+		return $locale_lcid[$language];
+	} else {
+		return false;
+	}
+}
+
+/**
  * Retrieves phpgettext variables from session
  *
  * @access		public
  * @param		none
- * @var
  * @return		array
- * @example
- * @see
  *
  * @author 		Damiano Venturin
- * @copyright 	2V S.r.l.
- * @license	GPL
  * @since		Sep 17, 2012
  */
 function getGettextSettings() {
@@ -119,14 +146,91 @@ function getGettextSettings() {
 	
 	$gettext_settings = array();
 	
-	$gettext_settings['locale'] = $CI->session->userdata('locale');
-	$gettext_settings['encoding'] = $CI->session->userdata('encoding');
-	$gettext_settings['supported_locales'] = $CI->session->userdata('supported_locales');
-	$gettext_settings['project_dir'] = $CI->session->userdata('project_dir');
+	$gettext_settings['locale'] = $CI->session->userdata('locale') ? $CI->session->userdata('locale') : 'en_US';
+	$gettext_settings['encoding'] = $CI->session->userdata('encoding') ? $CI->session->userdata('encoding') : 'UTF-8';
+	$gettext_settings['supported_locales'] = $CI->session->userdata('supported_locales') ? $CI->session->userdata('supported_locales') : array('en_US');
+	$gettext_settings['project_dir'] = $CI->session->userdata('project_dir') ? $CI->session->userdata('project_dir') : APPPATH.'third_party/php-gettext-1.0.11';
 	$gettext_settings['locale_dir'] = $CI->session->userdata('locale_dir');
-	$gettext_settings['default_locale'] = $CI->session->userdata('defaul_locale');
+	$gettext_settings['default_locale'] = $CI->session->userdata('defaul_locale') ? $CI->session->userdata('defaul_locale') : 'en_US';
+	
+	if(isset($CI->smarty) && is_object($CI->smarty)){
+		//sets locale as Smarty Global var
+		$CI->smarty->assignGlobal('locale', $gettext_settings['locale']);
+	}
 	
 	return $gettext_settings;
 }
+
+/**
+ * This function is written especially for Hero Framework.
+ * It translates the content dynamically retrieved from the database before sending it to smarty->display
+ * 
+ * @access		public
+ * @param		string $content
+ * @return		string or false
+ * 
+ * @author 		Damiano Venturin
+ * @license		GPL
+ * @link		http://www.venturin.net
+ * @since		Dec 8, 2012		
+ */
+function dynamically_translate_content($content , $add_random = false, $no_cache = false){
+
+	if(!is_string($content) || empty($content)) return false;
+	
+	$CI =& get_instance();
+	
+	//TODO if I make this check before calling dynamically_translate_content() I can 
+	//make this function more universal and flexible
+
+	//checks if location support is enabled
+	$setting = $CI->settings_model->get_setting('enable_localization');
+	if(!$setting['value']) return false;
+
+	//$html = $CI->smarty->fetch('string:'.$content);
+	$html = $CI->smarty->fetch('eval:'.$content);
+	return $html;	
+	
+/*	
+	// set a separate tpl_id for each unique URL
+	$tpl_id = md5($_SERVER['REQUEST_URI']);
+	if($add_random) $tpl_id .= uniqid('-');
+	$tpl_file = $tpl_id.'.thtml';
+	$tpl_file_full_path = $CI->config->item('path_templates_localization') . $tpl_file;
+		
+	$CI->load->helper('file');
+	
+	$cache_original_status = $CI->smarty->caching;
+	
+	//$CI->smarty->caching = 1;
+	$CI->smarty->setCaching(Smarty::CACHING_LIFETIME_SAVED);
+	$CI->smarty->setCacheLifetime(300);
+	
+	
+	if(!$CI->smarty->isCached($tpl_file_full_path) || $no_cache){
+		//writes the content to a temporary template which will be parsed by smarty->fetch: 
+		//if the content contains {t}{/t} blocks Smarty will try to translate the content and will
+		//remove the {t}{/t} blocks in any case returning a clean html without {t}{/t} block
+		if(write_file($tpl_file_full_path, $content, 'w+')){
+	
+			$html = $CI->smarty->fetch($tpl_file_full_path);
+	
+			if(!$cache_original_status) $CI->smarty->setCaching(Smarty::CACHING_OFF);
+			
+			return $html;
+			
+		} else {
+			
+			log_message('debug', 'The temporary template ' . $tpl_id . ' can not be written in '.$tpl_file_full_path . '. Please check filesystem permissions.');
+			return false;
+		}
+	} else {
+		$html = $CI->smarty->fetch($tpl_file_full_path);
+		if(!$cache_original_status) $CI->smarty->setCaching(Smarty::CACHING_OFF);
+		return $html;
+	}	
+*/
+}
+
 /* End of file phpgettext_helper.php */
 /* Location: ./application/helpers/phpgettext_helper.php */
